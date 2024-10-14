371b3805c85f45cab6b10f5e076e37075da9d452
JoseMgz <joseutp14@gmail.com>
2024-10-14 09:56:28 -0500

    feat: Se agregaron iconos

---
+++ src/utils/icon-stack.ts
@@ -0,0 +1,61 @@
+import {
+  NextjsIcon,
+  HTML5Icon,
+  CSSIcon,
+  JavaScriptIcon,
+  TypeScriptIcon,
+  ReactIcon,
+  TailwindCSSIcon,
+  NodejsIcon,
+  CSharpIcon,
+  FirebaseIcon,
+  SQLServerIcon,
+  ExpressjsIcon,
+  GithubIcon,
+  GitIcon,
+  NPMIcon,
+  PnpmIcon,
+  IllustratorIcon,
+  DockerIcon,
+  VisualStudioIcon,
+  VisualStudioCodeIcon,
+  PythonIcon,
+  MicrosoftAzureIcon,
+  AngularIcon,
+  MaterialUIIcon
+} from '@/components/icons';
+
+export const My_Stack = {
+  frontend: [
+    { name: 'HTML', icon: HTML5Icon },
+    { name: 'CSS', icon: CSSIcon },
+    { name: 'JavaScript', icon: JavaScriptIcon },
+    { name: 'TypeScript', icon: TypeScriptIcon },
+    { name: 'React.js', icon: ReactIcon },
+    { name: 'Tailwind', icon: TailwindCSSIcon },
+    { name: 'MaterialUI', icon: MaterialUIIcon }
+  ],
+  backend: [
+    { name: 'Next.js', icon: NextjsIcon },
+    { name: 'Node.js', icon: NodejsIcon },
+    { name: 'C#', icon: CSharpIcon },
+    { name: 'Firebase', icon: FirebaseIcon },
+    { name: 'SqlServer', icon: SQLServerIcon },
+    { name: 'ExpressJS', icon: ExpressjsIcon }
+  ],
+  tools: [
+    { name: 'Git', icon: GitIcon },
+    { name: 'GitHub', icon: GithubIcon },
+    { name: 'Illustrator', icon: IllustratorIcon },
+    { name: 'NPM', icon: NPMIcon },
+    { name: 'PNPM', icon: PnpmIcon },
+    { name: 'VisualStudioCode', icon: VisualStudioCodeIcon },
+    { name: 'VisualStudio', icon: VisualStudioIcon }
+  ],
+  learning: [
+    { name: 'Docker', icon: DockerIcon },
+    { name: 'Python', icon: PythonIcon },
+    { name: 'Microsoft Azure', icon: MicrosoftAzureIcon },
+    { name: 'Angular', icon: AngularIcon }
+  ]
+};
