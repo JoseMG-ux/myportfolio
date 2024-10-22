8a9608a32ca95c8f5d1d4bd55fd555c0778ac20a
JoseMgz <joseutp14@gmail.com>
2024-10-22 15:09:51 -0500

    feat: Se agrego seccion Sobre Mi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Snippet, Code, Link } from '@nextui-org/react';
+import { Avatar, Link } from '@nextui-org/react';
 import { button as buttonStyles } from '@nextui-org/theme';
 import { BusinessCenter, SentimentVerySatisfied, IntegrationInstructions } from '@mui/icons-material';
 
@@ -8,6 +8,7 @@ import { GithubIcon, LinkedInIcon } from '@/components/icons';
 import DefaultLayout from '@/layouts/default';
 import CardWorkExperience from '@/components/card-work-experience';
 import CardDevelopmentSkills from '@/components/card-development-skills';
+import AboutMe from '@/components/about-me';
 import { My_Stack } from '@/utils/icon-stack';
 
 export default function IndexPage() {
@@ -49,10 +50,11 @@ export default function IndexPage() {
                 <p className="text-medium text-foreground/80">
                   Actualmente, contribuyo al desarrollo y mejora del sistema de gestión, soporte y administración de Facturación
                   Electrónica. Integro nuevas funcionalidades y un diseño atractivo para la mejora de la experiencia para los usuarios
-                  actuales. Utilizamos <strong className="underline decoration-pink-500">React</strong> junto con{' '}
-                  <strong className="underline decoration-pink-500">Next</strong> y Typescript para el Frontend, mientras que los servicios{' '}
-                  <strong className="underline decoration-pink-500">API REST</strong> están desarrollados en
-                  <strong className="underline decoration-pink-500"> C# .NET</strong> y alojados en la nube de Microsoft Azure.
+                  actuales. Utilizamos <strong className="underline decoration-blue-500">React</strong> junto con{' '}
+                  <strong className="underline decoration-blue-500">Next</strong> y{' '}
+                  <strong className="underline decoration-cyan-500">Typescript</strong> para el Frontend, mientras que los servicios{' '}
+                  <strong className="underline decoration-green-500">API REST</strong> están desarrollados en
+                  <strong className="underline decoration-purple-500"> C# .NET</strong> y alojados en la nube de Microsoft Azure.
                 </p>
               }
               content_company={'PanamaPAC Solutions and Services, S.A.'}
@@ -61,35 +63,45 @@ export default function IndexPage() {
             />
           </div>
         </div>
-        <div className="flex gap-3">
-          <div className="flex flex-row justify-center">
-            <h2 className="flex items-center text-2xl font-bold gap-x-3 text-black/80 dark:text-white">
-              <IntegrationInstructions fontSize={'medium'} />
-              Habilidades
-            </h2>
+        <div className="container mt-12">
+          <div className="flex gap-4">
+            <div className="flex flex-row justify-center">
+              <h2 className="flex items-center text-2xl font-bold gap-x-3 text-black/80 dark:text-white">
+                <SentimentVerySatisfied fontSize={'medium'} />
+                Sobre mí
+              </h2>
+            </div>
+          </div>
+          <div className="container mt-4">
+            <AboutMe
+              content_body={
+                <p className="text-medium text-foreground/80">
+                  Hola, Soy <strong className="font-bold"> José Moreno Gómez</strong>, desarrollador Web. Con +2 años de experiencia en el
+                  desarrollo web. <br /> Me especializo en tecnologías como <strong className="text-cyan-400">React.js</strong>,{' '}
+                  <strong className="text-yellow-400">JavaScript</strong> y <strong className="text-blue-400">TypeScript</strong> para el
+                  Frontend como tambien tengo conocimientos en desarrollado Backend. <br /> Me dedico a crear soluciones de software que
+                  optimicen procesos y mejoren la experiencia de los usuarios. Mi enfoque está en la innovación y la eficiencia, siempre
+                  buscando ofrecer resultados que generen un impacto real.
+                </p>
+              }
+            />
           </div>
         </div>
-        <div className="flex flex-wrap justify-center gap-4 sm:gap-8 md:gap-10 lg:gap-12 xl:gap-16">
-          <CardDevelopmentSkills skillStack={My_Stack.frontend} title={'Frontend'} />
-          <CardDevelopmentSkills skillStack={My_Stack.backend} title={'Backend'} />
-          <CardDevelopmentSkills skillStack={My_Stack.tools} title={'Herramientas'} />
-          <CardDevelopmentSkills skillStack={My_Stack.learning} title={'Aprendiendo'} />
-        </div>
-
-        <div className="flex gap-3">
-          <div className="flex flex-row justify-center">
-            <h2 className="flex items-center text-2xl font-bold gap-x-3 text-black/80 dark:text-white">
-              <SentimentVerySatisfied fontSize={'medium'} />
-              Sobre mí
-            </h2>
+        <div className="container mt-12">
+          <div className="flex">
+            <div className="flex flex-row justify-center">
+              <h2 className="flex items-center text-2xl font-bold gap-x-3 text-black/80 dark:text-white">
+                <IntegrationInstructions fontSize={'medium'} />
+                Habilidades
+              </h2>
+            </div>
           </div>
-        </div>
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              <Code color="primary">pages/index.tsx</Code>
-            </span>
-          </Snippet>
+          <div className="flex flex-wrap justify-center gap-4 sm:gap-8 md:gap-10 lg:gap-12 xl:gap-16">
+            <CardDevelopmentSkills skillStack={My_Stack.frontend} title={'Frontend'} />
+            <CardDevelopmentSkills skillStack={My_Stack.backend} title={'Backend'} />
+            <CardDevelopmentSkills skillStack={My_Stack.tools} title={'Herramientas'} />
+            <CardDevelopmentSkills skillStack={My_Stack.learning} title={'Aprendiendo'} />
+          </div>
         </div>
       </section>
     </DefaultLayout>
