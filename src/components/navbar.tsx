35cca20a123acb940250c37c8f3d7b3ad31ce206
JoseMgz <joseutp14@gmail.com>
2024-11-02 08:44:20 -0500

    refactor: Se modifico url de inicio y se elimino codigo innecesario

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,34 +1,21 @@
 import { Link } from '@nextui-org/link';
 import { Navbar as NextUINavbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle } from '@nextui-org/navbar';
+import { Code } from '@mui/icons-material';
 
 import { siteConfig } from '@/config/site';
 import { ThemeSwitch } from '@/components/theme-switch';
 import { GithubIcon } from '@/components/icons';
-import { Logo } from '@/components/icons';
 
 export const Navbar = () => {
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand className="gap-3 max-w-fit">
-          <Link className="flex justify-start items-center gap-1" color="foreground" href="/">
-            <Logo />
+          <Link className="flex justify-start items-center gap-1" color="foreground" href="/myportfolio/">
+            <Code />
             <p className="font-bold text-inherit">Mi Portafolio</p>
           </Link>
         </NavbarBrand>
-        {/* <div className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <Link
-                className={clsx(linkStyles({ color: 'foreground' }), 'data-[active=true]:text-primary data-[active=true]:font-medium')}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </Link>
-            </NavbarItem>
-          ))}
-        </div> */}
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex basis-1/5 sm:basis-full" justify="end">
